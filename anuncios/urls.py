6467b3636615a0a55b3c2d9abf9bff1e66a9ef4d
Francisco Matelli <tercmt-francisco@ftd.com.br>
2023-07-18 21:10:13 -0300

    habilita API nativa

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-#from .views import (
-#    TodoListApiView,
-#)
+from .views import (
+    TodoListApiView,
+)
 
 urlpatterns = [
-    path("", views.index, name="index"),
-#    path('api', TodoListApiView.as_view()),
+    path("json", views.index),
+    path("", TodoListApiView.as_view(), name="index"),
+    path("render",  views.renderHTML)
 ]
--- anuncios/views.py
+++ anuncios/views.py
@@ -86,4 +86,29 @@ def path_to_json_without_nested(path):
             [path_to_json_without_nested(os.path.join(path,x)) for x in os.listdir\
                                (path)]
 
+def renderHTML(refer):
+    global GLOBAL_VAR_X
+    GLOBAL_VAR_X="["
+
+    for i in os.scandir(path):
+        if i.is_file() and i.path.lower().endswith(('.jpg')) and not i.path.lower().endswith(('_lowquality.jpg')):
+            #Using UNIX filesystem
+            imagem_lowquality = i.path[0:-4] + "_lowquality.jpg"
+            if not os.path.isfile(imagem_lowquality) :
+                foo = Image.open(i.path) 
+                foo = foo.resize((300,300),Image.ANTIALIAS)
+                foo.save(imagem_lowquality, optimize=True, quality=75)
+            
+            #GLOBAL_VAR_X.append('<p>' + trocadonew + '</p>')
+            #trocado = i.path
+            trocado = imagem_lowquality.replace(path_trocar, "/static/")
+
+            GLOBAL_VAR_X.append('<img src="' + trocado + '" width="200">') 
+        elif i.is_dir():
+            trocado = i.path.replace(path_trocar + "usados/", "")
+            GLOBAL_VAR_X.append('<h3>' + trocado + '</h3>')
+            dir_scan(i.path)
+
+
+    return HttpResponse(json_object)
 #print json.dumps(path_to_dict('.'))
