f1d549008d2cfdb40b991093f4fac70b2017f815
Francisco Matelli <tercmt-francisco@ftd.com.br>
2023-07-22 21:43:32 -0300

    Refatora e limpa codigo

--- anuncios/views-old.py
+++ anuncios/views-old.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from PIL import Image
 import json
-import string
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,35 +18,55 @@ from django.core import serializers
 
 global GLOBAL_VAR_X
 
-path = "/code/anuncios-controle/usados/"
+path = "/code/anuncios-controle/anuncios-prontos/usados/"
 
 class TodoListApiView(APIView):
+    # add permission to check if user is authenticated
+    #permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
     def get(self, request, *args, **kwargs):
-        json_dir = json.dumps( path_to_json(path) )
+        '''
+        List all the todo items for given requested user
+        '''
+        #todos = Todo.objects.filter(user = request.user.id)
+        #serializer = TodoSerializer(todos, many=True)
+        global GLOBAL_VAR_X
+        GLOBAL_VAR_X="["
+        json_dir = json.dumps(path_to_json(path))
+        path_to_json_without_nested(path)
+        json_dir2  = GLOBAL_VAR_X;
+        json_dir2 = GLOBAL_VAR_X[0:-1]+"]"
         json_object = json.loads(json_dir)
         return Response(json_object, status=status.HTTP_200_OK)
 
 
 def index(request):
-    json_dir = json.dumps( path_to_json(path) )
+    global GLOBAL_VAR_X
+    
+    #path = static 
+    #dir_scan(path)
+
+    #dd()
+    json_dir = json.dumps(path_to_json(path))
+    GLOBAL_VAR_X="["
+    path_to_json_without_nested(path)
+    json_dir2  = GLOBAL_VAR_X;
+    json_dir2 = GLOBAL_VAR_X[0:-1]+"]"
+    
+    #return JsonResponse(json_dir2, safe=False)
     return HttpResponse(json_dir, content_type="application/json")
 
 def path_to_json(path):
-    if os.path.basename(path)!=".DS_Store":
-        d = {'name': os.path.basename(path)}
-        d['path'] = path
-        d['x'] = path.count('/')-3
-        if os.path.isdir(path):
-            d['type'] = "directory"
-            #usar o reverso só nas fotos do produto, não na listagem
-            if (path.count('|')):
-                d['children'] = [path_to_json(os.path.join(path,x)) for x in  sorted(os.listdir(path), key=str.casefold ) if not x.startswith('.') ]
-            else:
-                d['children'] = [path_to_json(os.path.join(path,x)) for x in  sorted(os.listdir(path), key=str.casefold ) if not x.startswith('.') ]
-        else:
-            d['type'] = "file"
+    d = {'name': os.path.basename(path)}
+    d['path'] = path
+    d['x'] = path.count('/')-3
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [path_to_json(os.path.join(path,x)) for x in os.listdir\
+                         (path)]
     else:
-        d = {'name': 'nps'}
+        d['type'] = "file"
     return d
 
 def path_to_json_without_nested(path):
